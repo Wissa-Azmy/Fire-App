cd2cb98edcc8c8b26a05ff049c357151ed26e377
Wissa Azmy <wissa.azmy@gmail.com>
2019-05-06 14:42:55 +0200

    Initial Commit

---
+++ Fire App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fire App
+//
+//  Created by Wissa Azmy on 5/6/19.
+//  Copyright © 2019 Wissa Azmy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
