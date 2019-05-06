b013a6e8c1ece9c734501dc578cc8e70d6b258fd
Wissa Azmy <wissa.azmy@gmail.com>
2019-05-06 19:55:09 +0200

    Add Register & Login

--- Fire App/ViewController.swift
+++ Fire App/ViewController.swift
@@ -7,8 +7,14 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var emailTxtField: UITextField!
+    @IBOutlet weak var passwordTxtField: UITextField!
+    @IBOutlet weak var loginBtn: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,5 +22,34 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func createUserBtnTapped(_ sender: UIButton) {
+        if let email = emailTxtField.text, let pass = passwordTxtField.text {
+            Auth.auth().createUser(withEmail: email, password: pass) { (response, error) in
+                print(response?.user.email ?? "User was not created.")
+                print(response?.user.uid ?? "No user id found.")
+            }
+        }
+    }
+    
+    
+    @IBAction func loginBtnTapped(_ sender: UIButton) {
+        if Auth.auth().currentUser == nil {
+            if let email = emailTxtField.text, let pass = passwordTxtField.text {
+                Auth.auth().signIn(withEmail: email, password: pass) { (user, error) in
+                    if error == nil {
+                        self.loginBtn.setTitle("Log Out", for: .normal)
+                    }
+                }
+            }
+        } else {
+            do {
+                try Auth.auth().signOut()
+                self.loginBtn.setTitle("Login", for: .normal)
+            } catch {}
+            
+        }
+    }
+    
+    
 }
 
