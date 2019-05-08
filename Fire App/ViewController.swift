25b8d0cfc7000df150190e1be60ec1728adafdbe
Wissa Azmy <wissa.azmy@gmail.com>
2019-05-08 14:33:02 +0200

    Code Refactor

--- Fire App/ViewController.swift
+++ Fire App/ViewController.swift
@@ -25,11 +25,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        authUI = FUIAuth.defaultAuthUI()
-        authUI?.delegate = self
-        let providers: [FUIAuthProvider] = [FUIGoogleAuth(), FUIEmailAuth()]
-        
-        authUI?.providers = providers
+        configureAuthUI()
         
         DBReference = Database.database().reference()
         StorageReference = Storage.storage().reference()
@@ -40,35 +36,124 @@ class ViewController: UIViewController {
         super.viewDidAppear(true)
         
         if Auth.auth().currentUser != nil {
-            DBReference.child("games").child("2").setValue(["name": "second", "score": 15])
-            // Updating db values
-//            DBReference.child("games").child("1").child("name").setValue("new name")
+            // Create Object
+            createObjectUsingFirebaseDatabase()
+            
+            // Update a value
+            updateFBDatabaseData()
+            
+            // Deleting a value
+//            deleteDatafromFBDatabase()
+        }
+    }
+
+
+    @IBAction func createUserBtnTapped(_ sender: UIButton) {
+//        createUserUsingFirebaseAuth()
+        uploadDataToFirebaseCloudStorage()
+    }
+    
+    
+    @IBAction func loginBtnTapped(_ sender: UIButton) {
+//        loginUsingFirebaseAuth()
+        loginUsingFirebaseAuthUI()
+    }
+    
+}
+
+
+
+
+
+// MARK: - Firebase Authentication
+extension ViewController: FUIAuthDelegate {
+    
+    func configureAuthUI() {
+        authUI = FUIAuth.defaultAuthUI()
+        authUI?.delegate = self
+        let providers: [FUIAuthProvider] = [FUIGoogleAuth(), FUIEmailAuth()]
+        authUI?.providers = providers
+    }
+    
+    func createUserUsingFirebaseAuth() {
+        if let email = emailTxtField.text, let pass = passwordTxtField.text {
+            Auth.auth().createUser(withEmail: email, password: pass) { (response, error) in
+                print(response?.user.email ?? "User was not created.")
+                print(response?.user.uid ?? "No user id found.")
+            }
+        }
+    }
+    
+    func loginUsingFirebaseAuthUI() {
+        if Auth.auth().currentUser == nil {
+            if let authVC = authUI?.authViewController() {
+                present(authVC, animated: true)
+            }
+        } else {
+            do {
+                try Auth.auth().signOut()
+                self.loginBtn.setTitle("Login", for: .normal)
+            } catch {}
+            
+        }
+    }
+    
+    func loginUsingFirebaseAuth() {
+        if Auth.auth().currentUser == nil {
+            if let email = emailTxtField.text, let pass = passwordTxtField.text {
+                Auth.auth().signIn(withEmail: email, password: pass) { (user, error) in
+                    if error == nil {
+                        self.loginBtn.setTitle("Log Out", for: .normal)
+                    }
+                }
+            }
+        } else {
+            do {
+                try Auth.auth().signOut()
+                self.loginBtn.setTitle("Login", for: .normal)
+            } catch {}
+        }
+    }
+    
+    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
+        if error == nil {
+            loginBtn.setTitle("Log out", for: .normal)
+        }
+    }
+}
+
+
+
+// MARK: - Firebase Database
+extension ViewController {
+    func createObjectUsingFirebaseDatabase() {
+        DBReference.child("games").child("2").setValue(["name": "second", "score": 15])
+    }
+    
+    func updateFBDatabaseData() {
+            DBReference.child("games").child("1").child("name").setValue("new name")
 //            DBReference.child("games/1/name").setValue("new name")
 //            DBReference.child("games/1").setValue(["name": "updated name", "score": 10])
 //            let childUpdates = ["games/1/name": "updated name", "games/1/score": nil] as [String: Any]
 //            DBReference.updateChildValues(childUpdates)
-            
-            // Deleting a value
-//            DBReference.child("games/1/name").setValue(nil)
-//            DBReference.child("games/1/name").removeValue()
-        }
     }
     
+    func deleteDatafromFBDatabase() {
+        DBReference.child("games/1/name").setValue(nil)
+//        DBReference.child("games/1/name").removeValue()
+    }
+}
+
+
+
+// MARK: - Firebase Cloud Storage
+extension ViewController {
     private func uploadDataToFirebaseCloudStorage(){
         let gamekey = DBReference.child("games/2").key  // Get object id
         let filename = "\(gamekey!).png"
         let fileReference = StorageReference.child(filename)
         let meta = StorageMetadata()
         meta.contentType = "image/png"
-        
-        // Upload a file using its URL whithout loading it to memory
-//        let fileURL = Bundle.main.url(forResource: "Moharebs", withExtension: "jpg")
-//
-//        fileReference.putFile(from: fileURL!, metadata: meta) { (meta, error) in
-//            if error == nil {
-//                self.DBReference.child("games/1/image").setValue(filename)
-//            }
-//        }
         
         // Uploading DataFile after loading its data to memory
         if let img = UIImage(named: "Steve.jpg") {
@@ -80,48 +165,22 @@ class ViewController: UIViewController {
             }
         }
     }
-
-
-    @IBAction func createUserBtnTapped(_ sender: UIButton) {
-        
-        uploadDataToFirebaseCloudStorage()
-        
-//        if let email = emailTxtField.text, let pass = passwordTxtField.text {
-//            Auth.auth().createUser(withEmail: email, password: pass) { (response, error) in
-//                print(response?.user.email ?? "User was not created.")
-//                print(response?.user.uid ?? "No user id found.")
-//            }
-//        }
-    }
     
     
-    @IBAction func loginBtnTapped(_ sender: UIButton) {
-        if Auth.auth().currentUser == nil {
-            if let authVC = authUI?.authViewController() {
-                present(authVC, animated: true)
+    private func uploadFileToFirebaseCloudStorage(){
+        let gamekey = DBReference.child("games/1").key  // Get object id
+        let filename = "\(gamekey!).png"
+        let fileReference = StorageReference.child(filename)
+        let meta = StorageMetadata()
+        meta.contentType = "image/png"
+        
+        // Upload a file using its URL whithout loading it to memory
+        let fileURL = Bundle.main.url(forResource: "Moharebs", withExtension: "jpg")
+        
+        fileReference.putFile(from: fileURL!, metadata: meta) { (meta, error) in
+            if error == nil {
+                self.DBReference.child("games/1/image").setValue(filename)
             }
-//            if let email = emailTxtField.text, let pass = passwordTxtField.text {
-//                Auth.auth().signIn(withEmail: email, password: pass) { (user, error) in
-//                    if error == nil {
-//                        self.loginBtn.setTitle("Log Out", for: .normal)
-//                    }
-//                }
-//            }
-        } else {
-            do {
-                try Auth.auth().signOut()
-                self.loginBtn.setTitle("Login", for: .normal)
-            } catch {}
-            
-        }
-    }
-    
-}
-
-extension ViewController: FUIAuthDelegate {
-    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
-        if error == nil {
-            loginBtn.setTitle("Log out", for: .normal)
         }
     }
 }
